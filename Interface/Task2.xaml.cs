28a48a584b661549c33d68bce3c12b0b1e40057f
Александр <mr.cfif2003@gmail.com>
2024-04-11 21:02:35 +0300

    Inetface change.

--- Interface/Task2.xaml.cs
+++ Interface/Task2.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,31 @@ namespace Interface
         public Task2()
         {
             InitializeComponent();
+        }
+
+        private void RunFactorization_Click(object sender, RoutedEventArgs e)
+        {
+            ResFactorization.Document.Blocks.Clear();
+            int number;
+            try
+            {
+                if (int.TryParse(NumberForFactorization.Text, out number) == false || int.Parse(NumberForFactorization.Text) <= 0)
+                {
+                    throw new Exception("Введите целое положительное число. Пример ввода: 1 23 521");
+                }
+                ResFactorization.AppendText(NumberLib.Factorization(int.Parse(NumberForFactorization.Text)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                NumberForFactorization.Text = string.Empty;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            NumberForFactorization.Text = string.Empty;
+            ResFactorization.Document.Blocks.Clear();
         }
     }
 }
