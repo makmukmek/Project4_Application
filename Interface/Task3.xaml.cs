cf5303d24d4fcba3824c10aee354d648be5a8fe0
Александр <mr.cfif2003@gmail.com>
2024-04-11 21:09:19 +0300

    Interface change.

--- Interface/Task3.xaml.cs
+++ Interface/Task3.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,30 @@ namespace Interface
         public Task3()
         {
             InitializeComponent();
+        }
+
+        private void RunPrimeDivisors_Click(object sender, RoutedEventArgs e)
+        {
+            ResPrimeDivisors.Document.Blocks.Clear();
+            int number;
+            try
+            {
+                if (int.TryParse(NumberForPrimeDivisors.Text, out number) == false || int.Parse(NumberForPrimeDivisors.Text) <= 2)
+                {
+                    throw new Exception("Введите целое положительное число большее двух. Пример ввода: 3 32 125");
+                }
+                ResPrimeDivisors.AppendText(NumberLib.PrimeDivisors(int.Parse(NumberForPrimeDivisors.Text)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                NumberForPrimeDivisors.Text = string.Empty;
+            }
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            NumberForPrimeDivisors.Text = string.Empty;
+            ResPrimeDivisors.Document.Blocks.Clear();
         }
     }
 }
