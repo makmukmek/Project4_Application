e6297842f1ec7c0b287c0ce20fa7263adfe3f743
Александр <mr.cfif2003@gmail.com>
2024-04-11 22:03:37 +0300

    Interface change.

--- Interface/Task5.xaml.cs
+++ Interface/Task5.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Numerics;
 
 namespace Interface
 {
@@ -22,6 +24,31 @@ namespace Interface
         public Task5()
         {
             InitializeComponent();
+        }
+
+        private void RunBigIntFactorization_Click(object sender, RoutedEventArgs e)
+        {
+            ResBigIntFactorization.Document.Blocks.Clear();
+            BigInteger number;
+            try
+            {
+                if (BigInteger.TryParse(NumberForBigIntFactorization.Text, out number) == false || BigInteger.Parse(NumberForBigIntFactorization.Text) <= 0)
+                {
+                    throw new Exception("Введите целое положительное число. Пример ввода: 1 23 521");
+                }
+                ResBigIntFactorization.AppendText(NumberLib.BigIntFactorization(BigInteger.Parse(NumberForBigIntFactorization.Text)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                NumberForBigIntFactorization.Text = string.Empty;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            NumberForBigIntFactorization.Text = string.Empty;
+            ResBigIntFactorization.Document.Blocks.Clear();
         }
     }
 }
